cbe568e4536743c7184c61cabfef030099656b40
johnnyk2000 <johnnyhenryk2000@gmail.com>
2022-07-22 13:50:55 -0700

    Update microservo.ino

--- microservo/microservo.ino
+++ microservo/microservo.ino
@@ -1,21 +1,34 @@
 
 #include <Servo.h>
 
-Servo microServo;
-int pos = 0; //servo position
-int val;
-int potpin = 0;
+Servo microServo1;
+Servo microServo2;
+int val1;
+int potpin1 = 0;
+
+int val2;
+int potpin2 = 0;
+
 
 void setup() {
-  microServo.attach(9);
+  microServo1.attach(9);
+  microServo2.attach(10);
+
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val = analogRead(potpin);
-  val = map(val, 0, 1023, 0, 250);
-  Serial.println(val);
-  microServo.write(val);
+  val1 = analogRead(potpin1);
+  val1 = map(val1, 0, 1023, 0, 250);
+  Serial.println(val1);
+  microServo1.write(val1);
+
+  val2 = analogRead(potpin2);
+  val2 = map(val2, 0, 1023, 0, 250);
+  Serial.println(val2);
+  microServo2.write(val2);
+  
   delay(200);
+  
 }
