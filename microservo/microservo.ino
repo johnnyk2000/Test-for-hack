f4c40d973d908b8ec53729119b3fb640f9282e61
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2022-07-23 15:11:31 -0700

    ServoArmsArduino

--- microservo/microservo.ino
+++ microservo/microservo.ino
@@ -1,34 +1,61 @@
+#include "BluetoothSerial.h"
+#include <Servo_ESP32.h>
 
-#include <Servo.h>
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)           //error catching
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-Servo microServo1;
-Servo microServo2;
-int val1;
-int potpin1 = 0;
+BluetoothSerial SerialBT; //Bluetooth Connection
 
-int val2;
-int potpin2 = 0;
+static const int leftArmPin = 14;
+static const int rightArmPin = 32;
 
+Servo_ESP32 leftArm;
+Servo_ESP32 rightArm;
+
+int leftServoAngle =90;
+int leftServo1AngleMin =0;
+int leftServo1AngleMax = 180;
+//int righttServoAngle =90;
+//int rightServo1AngleMin =0;
+//int rightServo1AngleMax = 180;
+
+char readPos;
 
 void setup() {
-  microServo1.attach(9);
-  microServo2.attach(10);
+  // put your setup code here, to run once:
+  //pinMode(rightArm, OUTPUT);
+  //pinMode(leftArm, OUTPUT);
+  Serial.begin(115200);
+  SerialBT.begin("Maniac arms");
+  Serial.println("Started successfully.");
 
-  Serial.begin(9600);
+  //digitalWrite(rightArm, LOW);
+ //digitalWrite(leftArm, LOW);
+
+  leftArm.attach(leftArmPin);
+  rightArm.attach(rightArmPin);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val1 = analogRead(potpin1);
-  val1 = map(val1, 0, 1023, 0, 250);
-  Serial.println(val1);
-  microServo1.write(val1);
+   if (SerialBT.available()) { //if there are bytes available in the buffer
+    //readIn = char(SerialBT.read()); //read in one and store it in readIn
+        SerialBT.write(Serial.read());
+  }
+  if (readPos == 'o') {
+    rightArm.write(leftServo1AngleMin);
+    leftArm.write(leftServo1AngleMin);
 
-  val2 = analogRead(potpin2);
-  val2 = map(val2, 0, 1023, 0, 250);
-  Serial.println(val2);
-  microServo2.write(val2);
-  
-  delay(200);
-  
+    Serial.println("Neutral Position");
+  }
+  else if (readPos == 'p') {
+        rightArm.write(leftServo1AngleMax);
+        leftArm.write(leftServo1AngleMax);
+    Serial.println("Lifted Position");
+
+  }
+
+  delay(100);
 }
