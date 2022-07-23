013add9d65e0bd5d8f5d09ee914954c6d57985ad
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2022-07-23 15:17:24 -0700

    Update microservo.ino

--- microservo/microservo.ino
+++ microservo/microservo.ino
@@ -59,3 +59,69 @@ void loop() {
 
   delay(100);
 }
+
+
+
+
+
+// PROCESSING CODE
+import processing.serial.*;  //imports library used for wireless comm with the HC-05
+Serial myPort;               // declares Serial Port object from library
+String Text = "Welcome";
+
+void setup()
+{
+ size(900,1000);                         // create window of this size
+ myPort=new Serial(this, "COM14", 115200);  //COM port will likely be different, try different ones until it works (usually one of COM3-COM7)
+ myPort.bufferUntil('\n');               //delays calling serialEvent unitl reaching '\n'
+}
+
+
+
+void serialEvent(Serial myPort){        //serialEvent is called whenever data is available
+  Text = myPort.readStringUntil('\n');   //reads Serial.println() from Arduino
+}
+
+
+int state = 0;
+int x = 0;
+
+
+void draw()
+{
+
+  background(237,240,241);        // sets background to a color, parameters correspond to an RGB code
+  fill(20,160,133);               // sets color we will use to fill shapes
+  stroke(33);                     // sets color used for border around shapes
+  strokeWeight(1);                // sets width of stroke when drawing shapes
+  
+ 
+  fill(0);
+  textSize(32);
+  text(Text, 400, 700);           //draws text at x,y
+    
+  if (state == 1) {
+    myPort.write('o');            //writes to the Bluetooth Serial port
+    state = 0;
+  }
+  else if (state == 2) { //MAY HAVE TO CHANGE STATE NUMBERS IN COMBINED PROGRAM
+    myPort.write('p');
+    state = 0;
+  }
+}
+  
+  void keyPressed() {               //called whenever a key is pressed, key is automatically set to the key that is pressed
+  
+  if(key == 'o')
+  {
+    //move forward
+    state = 1;
+  }
+  else if (key == 'p')
+  {
+    //move backward
+    state = 2;
+  }
+}
+  
+  
